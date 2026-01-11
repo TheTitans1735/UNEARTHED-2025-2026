afb371e000a6d6a5169cb2186accab07a8ed7e02
TheTitans1735 <herzogrobotics1@gmail.com>
2026-01-11 20:51:30 +0200

    Refine driving parameters in mamgura, cave, and skeleton functions for improved task execution

--- Ilan.py
+++ Ilan.py
@@ -58,26 +58,32 @@ async def unearth():
 
 async def mamgura():
     debug=True
-    await ilan.drive_straight(50,700)
+    await ilan.drive_straight(45,700)
     await ilan.wait_for_button(debug)
-    await ilan.run_front_motor_fast(100, 0.3) # ביצוע משימה 8
-    await ilan.run_front_motor_fast(-100, 0.35) # ביצוע משימה 8
-    await ilan.run_front_motor_fast(100, 0.3)
-    await ilan.run_front_motor_fast(-100, 0.35)
-    await ilan.run_front_motor_fast(100, 0.3)
-    await ilan.run_front_motor_fast(-100, 0.35)
+    await ilan.run_front_motor_fast(-85, 0.35) # ביצוע משימה 8
+    await ilan.run_front_motor_fast(85, 0.3) # ביצוע משימה 8
+    await ilan.run_front_motor_fast(-85, 0.35)
+    await ilan.run_front_motor_fast(85, 0.3)
+    await ilan.run_front_motor_fast(-85, 0.35)
+    await ilan.run_front_motor_fast(85, 0.4)
     await ilan.wait_for_button(debug)
-    await ilan.turn(18)
+    await ilan.turn(-32)
     await ilan.wait_for_button(debug)
-    await ilan.drive_straight(20,500)
+    await ilan.drive_straight(40,500)
     await ilan.wait_for_button(debug)
-    await ilan.run_back_motor(500,90)
+    await ilan.turn(-40)
+    await ilan.wait_for_button(debug)
+    await ilan.drive_straight(35,500)
+    await ilan.wait_for_button(debug)
+    await ilan.turn(80)
+    await ilan.wait_for_button(debug)
+    await ilan.run_back_motor(500,29)
     await ilan.wait_for_button(debug)
     await ilan.drive_straight(-5)
     await ilan.wait_for_button(debug)
     await ilan.drive_straight(5)
     await ilan.wait_for_button(debug)
-    await ilan.run_back_motor(-500, 90)
+    await ilan.run_back_motor(-500, 29)
     await ilan.wait_for_button(debug)
     await ilan.drive_straight(-20,500)
     await ilan.wait_for_button(debug)
@@ -257,7 +263,7 @@ async def skeleton():
     await ilan.wait_for_button(debug)
     await ilan.drive_straight(5,500)
     await ilan.wait_for_button(debug)
-    await ilan.drive_straight(-5,500)
+    await ilan.drive_straight(-15,500)
     # await ilan.drive_straight(-2.5,750)
     # await ilan.wait_for_button(debug)
     # await ilan.run_front_motor(500, -150)
@@ -290,27 +296,27 @@ async def cave():
     # await ilan.motor_front.run_time(1000, 10) # הגעה למשימות 3 ו-4
     # await ilan.run_front_motor(500, -90)
     await ilan.wait_for_button(debug)
-    await ilan.motor_back.run_time(1000, 3000)
+    await ilan.motor_back.run_time(1000, 3250)
     await ilan.wait_for_button(debug)
-    await ilan.drive_straight(8.5, 75)
+    await ilan.drive_straight(9, 75)
     # await ilan.drive_straight(22.5, 75)
     await ilan.wait_for_button(debug)
-    await ilan.run_front_motor(100, -65) # ביצוע משימות 3 ו-4
+    await ilan.run_front_motor(100, -63) # ביצוע משימות 3 ו-4
     await ilan.motor_back.run_time(-800, 3000)
 
     await ilan.wait_for_button(debug)
-    await ilan.drive_straight(-13, 125)
+    await ilan.drive_straight(-12, 125)
     await ilan.wait_for_button(debug)
     await ilan.run_front_motor(500,-45)
     await ilan.wait_for_button(debug)
-    await ilan.turn(-90, 150)
+    await ilan.turn(90, 150)
     await ilan.wait_for_button(debug)
-    await ilan.drive_straight(85, 700) #חזרה לבית האדום
+    await ilan.drive_straight(80, 700) #חזרה לבית האדום
 
 async def ship():
     await ilan.drive_straight(58, 500)
     await ilan.run_front_motor(speed=400, angle=180)
-    await ilan.run_back_motor_fast(100, 5.1)
+    await ilan.run_back_motor_fast(100, 6)
     await ilan.drive_straight(-11, 900)
     await ilan.run_front_motor(800, -280)
     await ilan.drive_straight(20, 500)
